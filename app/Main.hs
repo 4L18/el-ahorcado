d6744f12065e691ea212ada86d519100f5de4909
S35H47 <45127101+S35H47@users.noreply.github.com>
2022-06-24 17:24:38 +0100

    Add play functions

--- app/Main.hs
+++ app/Main.hs
@@ -12,35 +12,7 @@ data Rol = Verdugo | Ahorcado
 
 -- todo Answer
 newtype Answer a = Answer { ans :: String}
-data Category = Book | Movie | Person | Serie | Song deriving Show
-
-data Game = Game { player1 :: Player
-                 , player2 :: Player
-                 , rounds :: Rounds
-                 } deriving (Show)
-
-game :: State Game
---data State s a = State (a -> (a, s))
---runState (State f) s = f s
-
-setPlayer :: String -> State Game ()
-setPlayer name = do {
-                        old <- get
-                        put (player1 game = name)
-                    }
-
-getGameState :: State s s
-getGameState = State (\state -> (state, state))
-
-modifyGameState :: (s -> s) -> State s ()
-modifyGameState f = State (\state -> ((), f state))
-
-instance Monad (State s) where
-    return x = State (\s -> (x, s))
-    op >>= f = State h
-        where h state0 = let (val, state1) = runState op state0
-                             op1 = f val
-                         in runState op1 state1
+data Category = Book | Movie | Person | Serie | Song deriving (Show, Read)
 
 data Round = Round { verdugo :: Player
                              , ahorcado :: Player
@@ -52,25 +24,112 @@ data Round = Round { verdugo :: Player
                              } deriving (Show)
 type Rounds = [Round]
 
-main :: IO ()
-main = do {
-    initGame;
-    --startRound;
-    --putStrLn (player2 ++ " if you want to guess then type \"resolve\" else type a letter");
-    --getLine >>= (\input -> if (map toLower input == "resolve")
-      --                     then inputResolve answer
-        --                   else if (length input == 1)
-          --                      then inputLetter (Just input) answer
-            --                    else inputLetter Nothing answer
-              --  )
-}         
+data Game = Game { player1 :: Player
+                 , player2 :: Player
+                 , rounds :: Rounds
+                 } deriving (Show)
 
-initGame :: GameState
+initGame :: IO Game
 initGame = do {
     putStrLn "Hello, players!";
     putStrLn "Player 1, enter your name";
-    player1 <- getLine;
+    name1 <- getLine;
     putStrLn "Player 2, enter your name";
-    player2 <- getLine;
-    return
+    name2 <- getLine;
+    return Game {player1 = Player name1 0, 
+        player2 = Player name2 0, rounds=[]}
+}
+
+initRound :: Int -> Player -> Player -> IO Round
+initRound numRonda (Player name1 _) (Player name2 _) = do {
+    verdugo = if mod round 2 == 0 then Player name1 0 else Player name2 0
+    putStrLn $ name1 ++ ", choose a category";
+    category <- getLine;
+    if category 
+    putStrLn $ name1 ++ ", introduce a " ++ category ++ " and don't let " ++ player2 ++ " see it!";
+    answer <- getLine;
+    return (Round )
+    --clearScreen;
+}
+
+#playRound :: Round -> IO Round
+
+
+playGame :: Game -> Int -> IO Game
+playGame (Game p1 p2 rounds) round = do
+    putStrLn $ "Playing round " ++ show round
+    if mod round 2 == 0 then
+        playRound (Round p2, p1)
+    else
+        playRound (Round p1, p2)
+
+    putStr "Do you want to play another round (y/n): "
+    res <- getLine
+    if res == "y" then
+        playGame (Game p1 p2 rounds) (round+1)
+    else
+        return (Game p1 p2 rounds)
+
+main = do
+    game <- initGame
+    gameResult <- playGame game 1
+    putStrLn "Bye"
+
+
+guess :: [Char] -> Maybe String
+guess s = if (s == "resolve")
+    then resolve
+    else if (size s == 1) 
+         then isCharPresent s
+         else Nothing
+    else Nothing
+
+
+resolve :: String -> Maybe String
+resolve answer = do {
+    putStr "Enter your awnser"
+    guess <- getLine
+    if guess == answer
+    then do {
+        putStr "You win"
+        return Nothing
+    }
+    else updateHang Nothing
+    updateRound
+}
+
+
+isCharPresent Char String:: IO ()
+isCharPresent c answer= do {
+    if (any (c==) answer)
+    then updatePanel c answer
+    else updateHang c
+    updateRound
+}
+
+
+updatePanel :: Char -> String -> String
+updatePanel letter [] = []
+updatePanel letter x:xs = if letter == x
+                          then x:(updatePanel xs)
+                          else "_":(updatePanel xs)
+
+
+updateHang :: Maybe Char -> Int
+updateHang Just c =  do {
+                     triedLetters ++ c;
+                     strikes++;
+                     return strikes;
+                     }
+updateHang Nothing = do {
+                     strikes++;
+                     return strikes;
+                     }
+
+
+updateRound ::
+updateRound = do {
+    showHangingMan
+    showTriedLetters
+    showPanel
 }
