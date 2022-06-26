9360ee632a21c8ac8c0320a0220f0ee0f7ceddcf
S35H47 <45127101+S35H47@users.noreply.github.com>
2022-06-26 10:47:24 +0100

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,30 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use print" #-}
+{-# LANGUAGE BlockArguments #-}
+{-# HLINT ignore "Use lambda-case" #-}
+
 module Main where
 
-import System.Console.ANSI
-import Data.Char
-import Control.Monad.Trans.State.Lazy
+import System.Console.ANSI ( clearScreen )
+import Data.Char (toUpper)
+import Control.Monad.Trans.State.Lazy ()
+import Data.Maybe (isNothing)
+
 
 type Name = String
 type Score = Int
-data Player = Player Name Score deriving Show
+data Player = Player { name :: Name, score :: Score} deriving Show
 
-data Rol = Verdugo | Ahorcado
-
--- todo Answer
-newtype Answer a = Answer { ans :: String}
-data Category = Book | Movie | Person | Serie | Song deriving (Show, Read)
+data Category = BOOK | MOVIE | PERSON | SERIE | SONG deriving (Show, Read)
 
 data Round = Round { verdugo :: Player
-                             , ahorcado :: Player
-                             , category :: Category
-                             , answer :: String
-                             , triedLetters :: [Char]
-                             , panel :: [Char]
-                             , strikes :: Int
-                             } deriving (Show)
+                   , ahorcado :: Player
+                   , category :: Category
+                   , answer :: String
+                   , letters :: [Char]
+                   , panel :: [Char]
+                   , strikes :: Int
+                   } deriving (Show)
 type Rounds = [Round]
 
 data Game = Game { player1 :: Player
@@ -29,123 +32,219 @@ data Game = Game { player1 :: Player
                  , rounds :: Rounds
                  } deriving (Show)
 
-initGame :: IO Game
-initGame = do {
-    putStrLn "Hello, players!";
-    putStrLn "Player 1, enter your name";
-    name1 <- getLine;
-    putStrLn "Player 2, enter your name";
-    name2 <- getLine;
-    return Game {player1 = Player name1 0, 
-        player2 = Player name2 0, rounds=[]}
-}
 
-initRound :: Int -> Player -> Player -> IO Round
-initRound numRonda (Player name1 _) (Player name2 _) = do {
-    verdugo = if mod round 2 == 0 then Player name1 0 else Player name2 0
-    putStrLn $ name1 ++ ", choose a category";
-    category <- getLine;
-    if category 
-    putStrLn $ name1 ++ ", introduce a " ++ category ++ " and don't let " ++ player2 ++ " see it!";
-    answer <- getLine;
-    return (Round )
-    --clearScreen;
-}
-
-#playRound :: Round -> IO Round
-
-
-playGame :: Game -> Int -> IO Game
-playGame (Game p1 p2 rounds) round = do
-    putStrLn $ "Playing round " ++ show round
-    if mod round 2 == 0 then
-        playRound (Round p2, p1)
-    else
-        playRound (Round p1, p2)
-
-    putStr "Do you want to play another round (y/n): "
-    res <- getLine
-    if res == "y" then
-        playGame (Game p1 p2 rounds) (round+1)
-    else
-        return (Game p1 p2 rounds)
-
+main :: IO ()
 main = do
     game <- initGame
     gameResult <- playGame game 1
     putStrLn "Bye"
 
 
-guess :: [Char] -> Maybe String
-guess s = if (s == "resolve")
-    then resolve
-    else if (size s == 1) 
-         then isCharPresent s
-         else Nothing
-    else Nothing
+initGame :: IO Game
+initGame = do
+    putStrLn "Hello, players!"
+    putStrLn "Player 1, enter your name"
+    name1 <- map toUpper <$> getLine
+    putStrLn "Player 2, enter your name"
+    name2 <- map toUpper <$> getLine
+    return Game { player1 = Player name1 0
+                , player2 = Player name2 0
+                , rounds = []
+                }
 
 
-resolve :: String -> Maybe String
-resolve answer = do {
-    putStr "Enter your awnser"
+playGame :: Game -> Int -> IO Game
+playGame (Game p1 p2 rounds) round = do
+    putStrLn $ "Playing round " ++ show round
+    roundStart <- initRound round p1 p2
+    rondaRes <- playRound roundStart 1
+    putStr "Do you want to play another round (y/n): "
+    res <- getLine
+    if res == "y" then
+        playGame (Game p1 p2 (rounds ++ [rondaRes])) (round+1)
+    else
+        return (Game p1 p2 (rounds ++ [rondaRes]))
+
+
+initRound :: Int -> Player -> Player -> IO Round
+initRound numRound (Player name1 _) (Player name2 _) = do
+    let verdugo = if even numRound then Player name1 0 else Player name2 0
+        ahorcado = if even numRound then Player name2 0 else Player name1 0
+    selectCategory verdugo >>= (\selectedCategory -> case selectedCategory of
+                                                        Nothing -> selectCategory verdugo
+                                                        (Just cat) -> do
+                                                            let category = cat
+                                                            requestAnswer verdugo ahorcado category
+                               )
+    answer <- map toUpper <$> getLine
+    clearScreen
+    return (Round verdugo ahorcado category answer [] (createPanel answer) 0)
+
+
+requestAnswer :: Player -> Player -> Category -> IO ()
+requestAnswer verdugo ahorcado category = putStrLn $ name verdugo ++ ", introduce a " ++ show category ++ " and don't let " ++ name ahorcado ++ " see it!"
+
+
+selectCategory :: Player -> Maybe Category
+selectCategory verdugo =
+    putStrLn $ name verdugo ++ ", choose one of these categories: book, movie, person, serie or song."
+    do
+        return if isCategory $ map toUpper (\input -> map toUpper <$> getLine)
+               then Just input
+               else Nothing
+
+
+isCategory :: String -> Bool
+isCategory s
+    | s `elem` ["BOOK", "MOVIE", "PERSON", "SERIE", "SONG"] = True
+    | otherwise = False
+
+
+createPanel :: String -> String
+createPanel [] = []
+createPanel (' ':xs) = ' ':createPanel xs
+createPanel (x:xs) = '_':createPanel xs
+
+
+playRound :: Round -> Int -> IO Round
+playRound (Round verdugo ahorcado category answer letters panel strikes) turn = do
+    putStrLn (name ahorcado ++ " if you want to guess then type \"resolve\" else type a letter")
+    input <- map toUpper <$> getLine
+    updatedRound <- updateRound input (Round verdugo ahorcado category answer letters panel strikes)
+    if strikes == 7
+    then do
+        putStrLn (name verdugo ++ " wins")
+        showUpdatedRound updatedRound
+        return updatedRound
+    else do
+        if answer == panel
+        then do
+            putStrLn (name ahorcado ++ " wins")
+            showUpdatedRound updatedRound
+            return updatedRound
+        else do
+            showUpdatedRound updatedRound
+            playRound updatedRound (turn+1)
+
+
+updateRound :: [Char] -> Round -> IO Round
+updateRound input (Round verdugo ahorcado category answer letters panel strikes)
+  | map toUpper input == "RESOLVE" = resolve (Round verdugo ahorcado category answer letters panel strikes)
+  | length input == 1 = isCharPresent (Round verdugo ahorcado category answer letters panel strikes) $ toUpper (head input)
+  | otherwise = do
+       putStrLn "Invalid input"
+       return (Round verdugo ahorcado category answer letters panel strikes)
+
+
+showUpdatedRound :: Round -> IO ()
+showUpdatedRound round = do
+    clearScreen
+    showHangingMan (strikes round)
+    showTriedLetters (letters round)
+    showPanel (panel round)
+
+
+resolve :: Round -> IO Round
+resolve (Round verdugo ahorcado category answer letters panel strikes) = do
+    putStrLn "Enter your awnser: "
     guess <- getLine
-    if guess == answer
-    then do {
-        putStr "You win"
-        return Nothing
-    }
-    else updateHang Nothing
-    updateRound
-}
+    return (if guess == answer
+            then Round verdugo ahorcado category answer letters panel strikes
+            else Round verdugo ahorcado category answer letters panel (strikes+1))
 
 
-isCharPresent Char String:: IO ()
-isCharPresent c answer= do {
-    if (any (c==) answer)
-    then updatePanel c answer
-    else updateHang c
-    updateRound
-}
+isCharPresent :: Round -> Char -> IO Round
+isCharPresent (Round verdugo ahorcado category answer letters panel strikes) c = do
+    return (if c `elem` answer
+            then Round verdugo ahorcado category answer letters (updatePanel c answer) strikes
+            else Round verdugo ahorcado category answer (letters ++ [c]) panel (strikes+1))
 
 
-updatePanel :: Char -> String -> String
-updatePanel letter [] = []
-updatePanel letter x:xs = if letter == x
-                          then x:(updatePanel xs)
-                          else "_":(updatePanel xs)
+updatePanel :: Char -> [Char] -> [Char]
+updatePanel _ [] = []
+updatePanel letter (x:xs)
+  | x == ' ' = ' ':updatePanel letter xs
+  | letter == x = x:updatePanel letter xs
+  | x /= '_' = x:updatePanel letter xs
+  | otherwise = '_':updatePanel letter xs
 
-
-updateHang :: Maybe Char -> Int
-updateHang Just c =  do {
-                     triedLetters ++ c;
-                     strikes++;
-                     return strikes;
-                     }
-updateHang Nothing = do {
-                     strikes++;
-                     return strikes;
-                     }
-
-
-updateRound ::
-updateRound = do {
-    showHangingMan strikes
-    showTriedLetters
-    showPanel
-}
 
 showHangingMan :: Int -> IO ()
-showHangingMan 1 = putStrLn "|"
-showHangingMan 2 = putStrLn "||"
-showHangingMan 3 = putStrLn "|||"
-showHangingMan 4 = putStrLn "||||"
-showHangingMan 5 = putStrLn "|||||"
-showHangingMan 6 = putStrLn "||||||"
-showHangingMan 6 = putStrLn "|||||||"
-showHangingMan _ = putStrLn "error"
+showHangingMan 1 = drawOneStrike
+showHangingMan 2 = drawTwoStrikes
+showHangingMan 3 = drawThreeStrikes
+showHangingMan 4 = drawFourStrikes
+showHangingMan 5 = drawFiveStrikes
+showHangingMan 6 = drawSixStrikes
+showHangingMan 7 = drawSevenStrikes
+showHangingMan _ = putStrLn ""
 
-showTriedLetters :: String -> IO ()
-showTriedLetters letters = putStrLn ("Tried letters:" ++ letters) 
+showTriedLetters :: [Char] -> IO ()
+showTriedLetters letters = putStrLn ("Tried letters: " ++ letters ++ "\n")
 
 showPanel :: String -> IO ()
-showPanel panel = putStrLn panel 
+showPanel panel = putStrLn $ "Panel: " ++ show panel ++ "\n"
+
+drawOneStrike :: IO ()
+drawOneStrike = do
+    putStrLn "  ___"
+    putStrLn " |   O"
+    putStrLn " |     "
+    putStrLn " |     "
+    putStrLn " |     "
+    putStrLn " |     "
+    putStrLn "_|_____"
+
+drawTwoStrikes :: IO ()
+drawTwoStrikes = do
+    putStrLn "  ___  "
+    putStrLn " |   O "
+    putStrLn " |   | "
+    putStrLn " |     "
+    putStrLn " |     "
+    putStrLn " |     "
+    putStrLn "_|_____"
+
+drawThreeStrikes :: IO ()
+drawThreeStrikes = do
+    putStrLn "  ___  "
+    putStrLn " |   O "
+    putStrLn " |  /| "
+    putStrLn " |     "
+    putStrLn " |     "
+    putStrLn "_|_____"
+
+drawFourStrikes :: IO ()
+drawFourStrikes = do
+    putStrLn "  ___  "
+    putStrLn " |   O "
+    putStrLn " |  /|\\"
+    putStrLn " |     "
+    putStrLn " |     "
+    putStrLn "_|_____"
+
+drawFiveStrikes :: IO ()
+drawFiveStrikes = do
+    putStrLn "  ___   "
+    putStrLn " |   O  "
+    putStrLn " |  /|\\"
+    putStrLn " |  /   "
+    putStrLn " |      "
+    putStrLn "_|_____ "
+drawSixStrikes :: IO ()
+drawSixStrikes = do
+    putStrLn "  ___   "
+    putStrLn " |   O  "
+    putStrLn " |  /|\\"
+    putStrLn " |  / \\"
+    putStrLn " |      "
+    putStrLn "_|_____ "
+
+drawSevenStrikes :: IO ()
+drawSevenStrikes = do
+    putStrLn "  ___   "
+    putStrLn " |   |  "
+    putStrLn " |   O  "
+    putStrLn " |  /|\\"
+    putStrLn " |  / \\"
+    putStrLn "_|_____ "
